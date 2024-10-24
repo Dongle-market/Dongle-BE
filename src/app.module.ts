c7f091c7df2a1f6d5348e3239a367f50a0536378
seungboshim <fodo9898@gmail.com>
2024-10-24 15:15:58 +0900

    chore: users 모듈화

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [UsersController],
+  imports: [UsersModule],
+  controllers: [AppController],
   // 컨트롤러 : express의 라우터와 같은 역할, url을 가져오고 함수를 실행
-  providers: [UsersService],
+  providers: [],
 })
 export class AppModule {}
