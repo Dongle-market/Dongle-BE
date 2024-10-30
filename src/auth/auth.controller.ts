778db17d39022ea0ba68c965577a1f047fbe1bcd
seungboshim <fodo9898@gmail.com>
2024-10-30 18:30:13 +0900

    feat: 인가코드 받아서 검증

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,34 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Response } from 'express';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('apis/auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Get()
-  async kakaoAuth(@Query("code") code: string) {
+  async kakaoAuth(@Query("code") code: string, @Res() res: Response) {
     const token = await this.authService.getKakaoToken(code);
     const user = await this.authService.getKakaoUserInfo(token);
-    return {token, user};
+    
+    const redirectUrl = `http://localhost:3001/auth?token=${token}&user=${JSON.stringify(user)}`;
+    res.redirect(redirectUrl)
+  }
+
+  @Post("login")
+  async login(@Body() authCode: string) {
+    const token = await this.authService.getKakaoToken(authCode);
+    const user = await this.authService.getKakaoUserInfo(token);
+    
+    const existingUser = await this.usersService.getOneByKakaoId(user.id.toString());
+
+    
+    if (existingUser) {
+
+    }
   }
 }
