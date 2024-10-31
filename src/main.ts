71bd7c3966b44e58632e054264f2f6e5b04c4e36
seungboshim <fodo9898@gmail.com>
2024-10-31 14:31:23 +0900

    feat: change port number

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3000;
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     transform: true, // 유저들이 보낸 데이터를 우리의 타입으로 변환해줌
   }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
+  console.log(`서버가 ${await app.getUrl()}번 포트에서 실행중입니다.`);
 }
 bootstrap();
