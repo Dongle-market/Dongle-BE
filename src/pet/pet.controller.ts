1e5671011bbd1b748386e1fc1d01fce429b5a8e5
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-11-03 16:39:40 +0900

    feat: pet api 에서 userId 헤더로부터 요청 받아오기

--- src/pet/pet.controller.ts
+++ src/pet/pet.controller.ts
@@ -1,36 +1,36 @@
 // pet.controller.ts
 
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Headers, Param, Post } from '@nestjs/common';
 import { PetService } from './pet.service';
 import { Pet } from './entities/pet.entity';
 import { CreatePetDto } from './dto/create-pet.dto';
 import { UpdatePetDto } from './dto/update-pet.dto';
 
-@Controller('apis/:userId/pet')
+@Controller('apis/pet')
 export class PetController {
   constructor(private readonly petService: PetService) {}
 
   /** 특정 유저의 반려동물 리스트 */
   @Get('mydongle')
-  async getAll(@Param('userId') userId: number): Promise<Pet[]> {
+  async getAll(@Headers('userId') userId: number): Promise<Pet[]> {
     return this.petService.getAllByUserId(userId);
   }
 
   /** 특정 유저의 특정 반려동물 정보 */
   @Get(':id')
-  async getOne(@Param('userId') userId: number, @Param('id') petId: number): Promise<Pet> {
+  async getOne(@Headers('userId') userId: number, @Param('id') petId: number): Promise<Pet> {
     return this.petService.getOneByUserId(userId, petId);
   }
 
   /** 특정 유저의 반려동물 등록 */
   @Post()
-  async create(@Param('userId') userId: number, @Body() petData: CreatePetDto): Promise<Pet> {
+  async create(@Headers('userId') userId: number, @Body() petData: CreatePetDto): Promise<Pet> {
     return await this.petService.create(userId, petData);
   }
 
   /** 특정 유저의 반려동물 삭제 */
   @Delete(':id')
-  async remove(@Param('userId') userId: number, @Param('id') petId: number): Promise<void> {
+  async remove(@Headers('userId') userId: number, @Param('id') petId: number): Promise<void> {
     return this.petService.deleteOneByUserId(userId, petId);
   }
 }
